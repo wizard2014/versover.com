bb03d0f2f4bd60d3ba1cf24dc74f9aeb6ced5800
wizard2014 <wizard2014@ya.ru>
2015-08-31 18:12:20 +0300

    add keywords, description

--- gulpfile.js
+++ gulpfile.js
@@ -29,7 +29,7 @@ gulp.task('styles', function() {
 });
 
 gulp.task('scripts', function() {
-    gulp.src([
+    return gulp.src([
         paths.bower   + '/bootstrap-hover-dropdown/bootstrap-hover-dropdown.js',
         paths.bower   + '/jquery-placeholder/jquery.placeholder.js',
         paths.bower   + '/matchHeight/jquery.matchHeight.js',
@@ -56,8 +56,6 @@ gulp.task('scripts', function() {
         .pipe(concat('app.js'))
         .pipe(uglify())
         .pipe(gulp.dest('./public/js'));
-
-    return gulp.src(paths.bower + '/loadcss/loadCSS.js').pipe(uglify()).pipe(gulp.dest('./public/js'));
 });
 
 gulp.task('watch', function() {
